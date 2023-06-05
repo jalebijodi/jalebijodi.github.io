5677d989470b8f120906b9b7acb3e6aa6ac8fe5a
Supriya Kankure <supriyakankure@Supriyas-MacBook-Air.local>
2023-06-04 23:50:39 -0400

    update

--- src/components/home-page/Consulting.js
+++ src/components/home-page/Consulting.js
@@ -13,7 +13,7 @@ const heading = '1:1 Consulting';
 const description = 'Navigating real-life scenarios, dating apps, and matrimony apps can be overwhelming, but with our expert consulting, we can help you navigate your situation effectively and be one step closer to your perfect match.';
 
 function OnClick() {
-  window.open('https://docs.google.com/forms/d/e/1FAIpQLSfqw0e4ac4TUOlOTYSISGGlhxS6R_6oftoksDLqERwNnwpIhA/viewform', "_blank");
+  window.open('https://calendly.com/jalebijodi/consulting-session?month=2023-06', "_blank");
 }
 
 function Btn() {
